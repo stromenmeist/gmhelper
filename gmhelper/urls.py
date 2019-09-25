2a4bb24101489aaa2872441aa132f54a1bf98181
stromenmeist <konradkowalke@gmail.com>
2019-09-25 14:36:24 +0200

    setup login page

--- gmhelper/urls.py
+++ gmhelper/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
     path('advplanner/', include('advplanner.urls')),
 
 ]
